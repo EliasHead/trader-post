63235a3f4bc13491aecf56e96c78f4940b10bd6a
eliashead <eliashead.costa@gmail.com>
2023-06-17 06:36:58 -0300

    feat: eslint  prettier plugin prettier/tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
-      <div className="flex gap-4 justify-center items-center container h-screen">
+      <div className="container flex h-screen items-center justify-center gap-4">
         <h1>Home</h1>
         <Link href="/matches">Jogos</Link>
         <Link href="/teams">Times</Link>
