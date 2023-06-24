8df33987ff9e369fd38fe0d73efdccdc5ae6e7bd
eliashead <eliashead.costa@gmail.com>
2023-06-24 06:54:44 -0300

    feat: fix table competition

--- src/components/competition/SearchCompetition.tsx
+++ src/components/competition/SearchCompetition.tsx
@@ -21,35 +21,35 @@ export default function SearchCompetition({ competitions }: any) {
         placeholder="What are you looking for?"
       />
       <div>
-        {competitionsFilter.map((competition: any) => {
-          return (
-            <div
-              key={competition.competition_id}
-              className="relative overflow-x-auto rounded-md"
-            >
-              <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
-                <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">
-                      Id
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Competição
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
+        <div className="relative overflow-x-auto rounded-md">
+          <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+            <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Id
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Competição
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {competitionsFilter.map((competition: any) => {
+                return (
+                  <tr
+                    key={competition.competition_id}
+                    className="border-b bg-white dark:border-gray-700 dark:bg-gray-800"
+                  >
                     <td className="px-6 py-4">{competition.competition_id}</td>
                     <td className="px-6 py-4">
                       {competition.competition_name}
                     </td>
                   </tr>
-                </tbody>
-              </table>
-            </div>
-          )
-        })}
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   )
