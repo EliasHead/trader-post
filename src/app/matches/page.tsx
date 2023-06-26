6ae1818ea585cc57eb54e9c4e1c9e167904e03ee
eliashead <eliashead.costa@gmail.com>
2023-06-25 21:32:31 -0300

    feat: select items

--- src/app/matches/page.tsx
+++ src/app/matches/page.tsx
@@ -1,11 +1,17 @@
 import MatchesList from '@/components/matches/MatchesList'
 import { AddMatches } from '@/components/matches/addMatches'
+import { prisma } from '@/utils/prisma'
+import { dataRounds } from '@/utils/rounds'
 
-export default function Matches() {
+export default async function Matches() {
+  const teams = await prisma.teams.findMany()
+  const competitions = await prisma.competition.findMany()
+  const rounds = dataRounds
+
   return (
     <div className="mt-12 flex h-screen w-full flex-col items-center justify-start gap-4">
       <h1>Jogos</h1>
-      <AddMatches />
+      <AddMatches teams={teams} competitions={competitions} rounds={rounds} />
       <MatchesList />
     </div>
   )
--- src/components/matches/addMatches.tsx
+++ src/components/matches/addMatches.tsx
@@ -2,16 +2,20 @@
 
 import { createMatches } from '@/utils/createMatches'
 import { ChangeEvent, FormEvent, useState } from 'react'
-export function AddMatches() {
+
+export function AddMatches({ teams, competitions, rounds }: any) {
   const [formData, setFormData] = useState({
     home_team: '',
     visitor_team: '',
     competition: '',
     round: '',
   })
+  console.log(teams)
 
   function handleChange(
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    event: ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >,
   ) {
     const { name, value } = event.target
     setFormData((prevState) => ({ ...prevState, [name]: value }))
@@ -23,82 +27,108 @@ export function AddMatches() {
 
   return (
     <form onSubmit={handleSubmit} className="w-full max-w-6xl">
-      <div className="mx-3 mb-6 flex items-center border-b border-teal-500 py-2">
+      <div className="mx-3 mb-6 flex items-end border-b border-teal-500 py-2">
         <div className="mb-6 w-full px-3 md:mb-0 md:w-1/2">
           <label
-            className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
             htmlFor="home_team"
+            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
           >
             Time da casa
           </label>
-          <input
-            type="text"
+          <select
             name="home_team"
             id="home_team"
             value={formData.home_team}
-            className="mb-3 block w-full appearance-none rounded bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-            placeholder="Time da casa"
-            aria-label="Time da casa"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             onChange={handleChange}
-          />
+          >
+            <option>Escolha o time</option>
+            {teams.map((team: any) => {
+              return (
+                <option key={team.team_id} value={team.team_id}>
+                  {team.team_name}
+                </option>
+              )
+            })}
+          </select>
         </div>
         <div className="mb-6 w-full px-3 md:mb-0 md:w-1/2">
           <label
-            className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
             htmlFor="visitor_team"
+            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
           >
             Time visitante
           </label>
-          <input
-            type="text"
+          <select
             name="visitor_team"
             id="visitor_team"
             value={formData.visitor_team}
-            className="mb-3 block w-full appearance-none rounded bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-            placeholder="Time visitante"
-            aria-label="Time visitante"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             onChange={handleChange}
-          />
+          >
+            <option>Escolha o time</option>
+            {teams.map((team: any) => {
+              return (
+                <option key={team.team_id} value={team.team_id}>
+                  {team.team_name}
+                </option>
+              )
+            })}
+          </select>
         </div>
         <div className="mb-6 w-full px-3 md:mb-0 md:w-1/2">
           <label
-            className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
             htmlFor="competition"
+            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
           >
             Competição
           </label>
-          <input
-            type="text"
+          <select
             name="competition"
             id="competition"
             value={formData.competition}
-            className="mb-3 block w-full appearance-none rounded bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-            placeholder="Competição"
-            aria-label="Competição"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             onChange={handleChange}
-          />
+          >
+            <option>Escolha a competição</option>
+            {competitions.map((competition: any) => {
+              return (
+                <option
+                  key={competition.competition_id}
+                  value={competition.competition_id}
+                >
+                  {competition.competition_name}
+                </option>
+              )
+            })}
+          </select>
         </div>
         <div className="mb-6 w-full px-3 md:mb-0 md:w-1/2">
           <label
-            className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
             htmlFor="round"
+            className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
           >
-            rodada
+            Rodada
           </label>
-          <input
-            type="text"
+          <select
             name="round"
             id="round"
             value={formData.round}
-            className="mb-3 block w-full appearance-none rounded bg-gray-200 px-4 py-3 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-            placeholder="Rodada"
-            aria-label="Rodada"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             onChange={handleChange}
-          />
+          >
+            <option>Escolha a Rodada</option>
+            {rounds.map((round: any) => {
+              return (
+                <option key={round.round_id} value={round.round_id}>
+                  {round.round_name}
+                </option>
+              )
+            })}
+          </select>
         </div>
-
         <button
-          className="text-md mt-2 flex-shrink-0 rounded border-4 border-teal-500 bg-teal-500 px-2 py-2 font-bold  text-white hover:border-teal-700 hover:bg-teal-700"
+          className="text-md mt-2 flex-shrink-0 rounded border-4 border-teal-500 bg-teal-500 px-2 py-1 font-bold  text-white hover:border-teal-700 hover:bg-teal-700"
           type="submit"
         >
           Adicionar
